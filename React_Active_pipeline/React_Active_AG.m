86254eadccb422184533dea6b5b47bf323795e40
Arsenii Goriachenkov <arsgorv@gmail.com>
2025-11-28 18:47:54 +0100

    update

--- React_Active_pipeline/React_Active_AG.m
+++ React_Active_pipeline/React_Active_AG.m
@@ -8,7 +8,40 @@ Arsenii Goriachenkov
 Paris, France
 %}
 
-%% 
+%% Select sessions
+% Form the list of sessions
+selection = 2;
+
+Dir{1} = PathForExperimentsReactActive({'Tvorozhok'}, 'head-fixed', 'none', '11_10');
+Dir{2} = PathForExperimentsReactActive({'Tvorozhok'}, 'head-fixed', 'none', '13_12');
+Dir{3} = PathForExperimentsReactActive({'Tvorozhok'}, 'head-fixed', 'none', 'all', 'perf_drop');
+
+% Dir{3} = PathForExperiments_React_Active_AG({'Kosichka'}, 'head-fixed', 'none');
+% Dir{4} = PathForExperiments_React_Active_AG({'Shropshire', 'Labneh', 'Brynza'}, 'head-fixed', 'none');
+Dir{5} = MergePathForExperiment(Dir{1},Dir{2});
+
+% Dirs_names = {'Tvorozhok', 'Kosichka', 'All animals', 'Arbitrary_Mix'};
+
+sessions = Dir{selection}.path';
+
+k = 1;
+session_dlc = {};
+
+for c = 1:length(sessions)
+    dlc_path = fullfile(sessions{c}, 'video'); 
+    files = dir(fullfile(dlc_path, '*_filtered.csv')); % Search for files ending with "_filtered.csv"
+    
+    if ~isempty(files) % Check if there are any matching files
+        session_dlc{k} = sessions{c}; % Store the session path
+        k = k + 1;
+    else
+        disp([Dir{selection}.path{c} ' - No DLC found']);
+    end
+end
+
+session_dlc = session_dlc';
+
+
 %% PreProcessing: Sleep Scoring
 Ferret_ProcessData_BM
 
--- React_Active_pipeline/analysis/React_Active_Behaviour_AG.m
+++ React_Active_pipeline/analysis/React_Active_Behaviour_AG.m
@@ -7,39 +7,6 @@ function React_Active_Behaviour_AG()
 % Under construction  - Study the correlation between OB/Cortical/Hippocampal gamma and pupil area (gamma_pupil_corr)
 % Under construction  - Producing the composition video with all variables synced (composition_video_OB_DLC_ferret)
 
-%% Select sessions
-% Form the list of sessions
-selection = 2;
-
-Dir{1} = PathForExperiments_React_Active_AG({'Tvorozhok'}, 'head-fixed', 'none', '11_10');
-Dir{2} = PathForExperiments_React_Active_AG({'Tvorozhok'}, 'head-fixed', 'none', '13_12');
-Dir{3} = PathForExperiments_React_Active_AG({'Tvorozhok'}, 'head-fixed', 'none', 'all', 'perf_drop');
-
-% Dir{3} = PathForExperiments_React_Active_AG({'Kosichka'}, 'head-fixed', 'none');
-% Dir{4} = PathForExperiments_React_Active_AG({'Shropshire', 'Labneh', 'Brynza'}, 'head-fixed', 'none');
-Dir{5} = MergePathForExperiment(Dir{1},Dir{2});
-
-% Dirs_names = {'Tvorozhok', 'Kosichka', 'All animals', 'Arbitrary_Mix'};
-
-sessions = Dir{selection}.path';
-
-k = 1;
-session_dlc = {};
-
-for c = 1:length(sessions)
-    dlc_path = fullfile(sessions{c}, 'video'); 
-    files = dir(fullfile(dlc_path, '*_filtered.csv')); % Search for files ending with "_filtered.csv"
-    
-    if ~isempty(files) % Check if there are any matching files
-        session_dlc{k} = sessions{c}; % Store the session path
-        k = k + 1;
-    else
-        disp([Dir{selection}.path{c} ' - No DLC found']);
-    end
-end
-
-session_dlc = session_dlc';
-
 %% -------------------------------------- PREPROCESSING -------------------------------------- 
 % quick version of sleep scoring, where I only calculate smooth brain powers
 for sess = 1:numel(session_dlc)
